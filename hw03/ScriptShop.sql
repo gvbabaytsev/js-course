668785a09a818accc8cf7d76f3f9766cec0003ae
GVBabaytsev <gvbabaytsev@greenatom.ru>
2023-03-23 00:00:48 +0300

    Edit homework 3

--- hw03/ScriptShop.sql
+++ hw03/ScriptShop.sql
@@ -13,27 +13,34 @@ CREATE SCHEMA IF NOT EXISTS  customers;
 CREATE SCHEMA IF NOT EXISTS  products;
 
 --Создаем таблицы
+CREATE TABLE IF NOT EXISTS employees.post(
+	post_id BIGSERIAL PRIMARY KEY,
+	name VARCHAR NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS employees.employee(
 	employee_id BIGSERIAL PRIMARY KEY,
-	employee_full_name VARCHAR NOT NULL,
+	full_name VARCHAR NOT NULL,
 	phone_number VARCHAR UNIQUE NOT NULL,
-	employee_email TEXT UNIQUE CHECK (employee_email ~ '\A\S+@\S+\.\S+\Z'),
-	employee_birthday DATE NOT NULL,
-	employee_passport_data TEXT NOT NULL
+	email TEXT UNIQUE CHECK (email ~ '\A\S+@\S+\.\S+\Z'),
+	birthday DATE NOT NULL,
+	passport_data TEXT NOT NULL,
+	post_id BIGSERIAL,
+	FOREIGN KEY (post_id) REFERENCES employees.post (post_id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS customers.customer(
 	customer_id BIGSERIAL PRIMARY KEY,
-	customer_full_name VARCHAR NOT NULL,
+	full_name VARCHAR NOT NULL,
 	phone_number VARCHAR UNIQUE NOT NULL,
-	customer_email TEXT UNIQUE CHECK (employee_email ~ '\A\S+@\S+\.\S+\Z'),
-	customer_birthday DATE NOT NULL
+	email TEXT UNIQUE CHECK (email ~ '\A\S+@\S+\.\S+\Z'),
+	birthday DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS products.product(
 	 product_id BIGSERIAL PRIMARY KEY,
-	 product_name VARCHAR UNIQUE NOT NULL,
-	 product_description TEXT,
+	 name VARCHAR UNIQUE NOT NULL,
+	 description TEXT,
 	 price INTEGER,
 	 weight INTEGER,
 	 manufacturer_country VARCHAR NOT NULL
